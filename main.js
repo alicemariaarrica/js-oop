067ede9ee4bde762c64bc409afae4d38e26733be
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-27 20:41:47 +0100

    comments-adj-&-istanza

--- main.js
+++ main.js
@@ -17,6 +17,19 @@ class Veicolo {
     }
 
 
-    informazioni() {
-        return `Marca: ${this.marca}, Anno: ${this.anno}, Colore: ${this.colore}, Porte: ${this.porte}, Carburante: ${this.carburante}`;
+//Creiamo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
+
+const miaVettura = new Veicolo("Fiat", 2019, "blu", 5, "benzina");
+
+//Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
+
+informazioni() {
+    return `Marca: ${this.marca}, Anno: ${this.anno}, Colore: ${this.colore}, Porte: ${this.porte}, Carburante: ${this.carburante}`;
+}
+//Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
+
+    calcolaEta() {
+        const annoCorrente = new Date().getFullYear();
+        return annoCorrente - this.anno;
     }
+}
